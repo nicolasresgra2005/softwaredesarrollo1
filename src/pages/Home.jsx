1f5d2cde7829a4acdbf88aeb9c6a6e9e7fe74e82
JuanFv3163 <margipa1975@gmail.com>
2025-10-16 11:18:42 -0500

    cambie las letras del inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 function Home() {
   return (
     <div className="info-section">
-      <h2>Bienvenido a <span className="highlight">AgroSense</span></h2>
+      <h2>Bienvenido a nicolas es una gonorrea <span className="highlight">AgroSense</span></h2>
       <p>
         Una plataforma inteligente que conecta tecnología y agricultura para optimizar tus recursos,
         monitorear tus cultivos y mejorar la producción de manera sostenible.
